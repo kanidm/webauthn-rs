504317e0b4abdc447c80bf3418fadb39c5e285e3
William Brown <william@blackhats.net.au>
2019-05-06 20:17:03 +1000

    Added cookie handling!

--- examples/actix/main.rs
+++ examples/actix/main.rs
@@ -8,8 +8,11 @@
 extern crate actix;
 extern crate actix_web;
 
+extern crate time;
+
 // #[macro_use]
 extern crate askama;
+extern crate cookie;
 extern crate env_logger;
 
 extern crate webauthn_rs;
@@ -20,7 +23,12 @@ use actix::prelude::*;
 use actix_web::{
     fs, http, middleware, server, App, Error, HttpRequest, HttpResponse, Json, Path, State,
 };
+// Bring in the trait for session()
+use actix_web::middleware::session::RequestSession;
 use futures::future::Future;
+
+use rand::prelude::*;
+use time::Duration;
 
 use webauthn_rs::ephemeral::WebauthnEphemeralConfig;
 use webauthn_rs::proto::{PublicKeyCredential, RegisterPublicKeyCredential};
@@ -42,7 +50,26 @@ impl AppState {
     }
 }
 
-fn index_view(_req: &HttpRequest<AppState>) -> HttpResponse {
+fn index_view(req: &HttpRequest<AppState>) -> HttpResponse {
+
+    let some_userid = match req.session().get::<String>("userid") {
+        Ok(v) => v,
+        Err(e) => {
+            return HttpResponse::InternalServerError().body("Internal Server Error");
+        }
+    };
+
+    println!("{:?}", some_userid);
+
+    if some_userid.is_none() {
+        match req.session().set("anonymous", true) {
+            Ok(_) => {},
+            Err(e) => {
+                return HttpResponse::InternalServerError().body("Internal Server Error");
+            }
+        }
+    };
+
     let s = IndexTemplate {
             // list: l,
         }
@@ -117,18 +144,30 @@ fn register(
 }
 
 fn login(
-    (lgn, username, state): (Json<PublicKeyCredential>, Path<String>, State<AppState>),
+    (lgn, username, state, req): (Json<PublicKeyCredential>, Path<String>, State<AppState>, HttpRequest<AppState>),
 ) -> impl Future<Item = HttpResponse, Error = Error> {
+
+    let uname = username.into_inner().clone();
+
     state
         .wan
         .send(Authenticate {
-            username: username.into_inner(),
+            username: uname.clone(),
             lgn: lgn.into_inner(),
         })
         .from_err()
-        .and_then(|res| {
+        .and_then(move |res| {
             match res {
-                Ok(_) => Ok(HttpResponse::Ok().json(())),
+                Ok(_) => {
+
+                    // Clear the anonymous flag
+                    req.session().remove("anonymous");
+                    // Set the userid
+                    match req.session().set("userid", uname) {
+                        Ok(_) => Ok(HttpResponse::Ok().json(())),
+                        Err(_) => Ok(HttpResponse::InternalServerError().body("Internal Server Error")),
+                    }
+                }
                 Err(_) => {
                     // TODO: Log this error
                     Ok(HttpResponse::InternalServerError().json(()))
@@ -141,22 +180,42 @@ fn main() {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
-    let sys = actix::System::new("checklists");
+    let sys = actix::System::new("webauthn-rs-demo");
+
+    let prefix = "/auth";
+
     let wan_c = WebauthnEphemeralConfig::new(
         "http://localhost:8080/auth",
         "http://localhost:8080",
         "localhost",
     );
+
     let wan = WebauthnActor::new(wan_c);
     let wan_addr = wan.start();
+
+    let mut stdrng = StdRng::from_entropy();
+    let cookie_sig: Vec<_> = (0..32).map(|_| stdrng.gen()).collect();
 
     // Start http server
     server::new(move || {
         App::with_state(AppState::new(wan_addr.clone()))
             // For production
-            .prefix("/auth")
+            .prefix(prefix)
             // enable logger
             .middleware(middleware::Logger::default())
+            .middleware(middleware::session::SessionStorage::new(
+                // Signed prevents tampering.
+                middleware::session::CookieSessionBackend::signed(&cookie_sig)
+                    // 
+                    .path(prefix)
+                    .domain("localhost")
+                    .same_site(cookie::SameSite::Strict)
+                    .name("webauthnrs")
+                    // if true, only allow to https
+                    .secure(false)
+                    // Valid for 5 minutes
+                    // .max_age(Duration::minutes(5))
+            ))
             .handler(
                 "/static",
                 fs::StaticFiles::new("./static")
