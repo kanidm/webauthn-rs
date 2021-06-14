4bb88dbe69c573e76f72d7d8de42cc55d8e5b807
William Brown <william@blackhats.net.au>
2021-06-14 15:12:10 +1000

    Add TryFrom to Base64UrlSafeData

--- src/base64_data.rs
+++ src/base64_data.rs
@@ -4,7 +4,15 @@
 
 use serde::de::{Error, Unexpected, Visitor};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::convert::TryFrom;
 use std::fmt;
+
+static ALLOWED_DECODING_FORMATS: &[base64::Config] = &[
+    base64::URL_SAFE_NO_PAD,
+    base64::URL_SAFE,
+    base64::STANDARD,
+    base64::STANDARD_NO_PAD,
+];
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 /// A container for binary that should be base64 encoded in serialisation. In reverse
@@ -33,14 +41,20 @@ impl AsRef<[u8]> for Base64UrlSafeData {
     }
 }
 
-struct Base64UrlSafeDataVisitor;
+impl TryFrom<&str> for Base64UrlSafeData {
+    type Error = ();
 
-static ALLOWED_DECODING_FORMATS: &[base64::Config] = &[
-    base64::URL_SAFE_NO_PAD,
-    base64::URL_SAFE,
-    base64::STANDARD,
-    base64::STANDARD_NO_PAD,
-];
+    fn try_from(v: &str) -> Result<Self, Self::Error> {
+        for config in ALLOWED_DECODING_FORMATS {
+            if let Ok(data) = base64::decode_config(v, *config) {
+                return Ok(Base64UrlSafeData(data));
+            }
+        }
+        Err(())
+    }
+}
+
+struct Base64UrlSafeDataVisitor;
 
 impl<'de> Visitor<'de> for Base64UrlSafeDataVisitor {
     type Value = Base64UrlSafeData;
@@ -80,5 +94,17 @@ impl Serialize for Base64UrlSafeData {
     {
         let encoded = base64::encode_config(&self, base64::URL_SAFE_NO_PAD);
         serializer.serialize_str(&encoded)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::base64_data::Base64UrlSafeData;
+    use std::convert::TryFrom;
+
+    #[test]
+    fn test_try_from() {
+        assert!(Base64UrlSafeData::try_from("aGVsbG8=").is_ok());
+        assert!(Base64UrlSafeData::try_from("abcdefghij").is_err());
     }
 }
