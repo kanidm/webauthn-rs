277084d373fbef72bdfb57d794acfeee0cf15078
Eric Mark Martin <ericmarkmartin@gmail.com>
2022-03-04 01:37:45 -0500

    clean up error

--- webauthn-rs-core/src/error.rs
+++ webauthn-rs-core/src/error.rs
@@ -85,9 +85,6 @@ pub enum WebauthnError {
     #[error("The attestation statement certInfo is not present")]
     AttestationStatementCertInfoMissing,
 
-    #[error("An extension for this identifier was not in the attestation statement")]
-    AttestationStatementMissingExtension,
-
     #[error("The attestation statement pubArea is not present")]
     AttestationStatementPubAreaMissing,
 
@@ -102,9 +99,6 @@ pub enum WebauthnError {
 
     #[error("The attestation Certificate's OID 1.3.6.1.4.1.45724.1.1.4 aaguid does not match the aaguid of the token")]
     AttestationCertificateAAGUIDMismatch,
-
-    #[error("The attestation Certificate's computed nonce does not match its OID 1.2.840.113635.100.8.2 value")]
-    AttestationCertificateNonceMismatch,
 
     #[error("The attestation created by the TPM is not correct")]
     AttestationTpmStInvalid,
