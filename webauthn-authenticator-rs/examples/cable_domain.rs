ec251e390aeeeef658c181d20a6ec596d966d1f7
Michael Farrell <micolous+git@gmail.com>
2023-04-14 11:49:53 +1000

    add DNS resolution

--- webauthn-authenticator-rs/examples/cable_domain.rs
+++ webauthn-authenticator-rs/examples/cable_domain.rs
@@ -1,15 +1,19 @@
-use clap::{ArgGroup, CommandFactory, Parser};
+use clap::{ArgGroup, CommandFactory, Parser, ValueHint};
+use std::net::ToSocketAddrs;
 use webauthn_authenticator_rs::cable::get_domain;
 
 #[derive(Debug, clap::Parser)]
 #[clap(
-    about = "caBLE tunnel server domain list",
+    about = "Derives caBLE tunnel server hostnames",
     after_help = "\
-When exactly one tunnel server ID is requested, only the hostname will be \
+When deriving exactly one tunnel server hostname, only the hostname will be \
 printed, and an unknown ID will result in an error.
 
-When multiple (or all) IDs are requested, the output will be in CSV format, \
-and any unknown tunnel IDs will result in an empty string.",
+When deriving multiple (or all) tunnel server hostnames, or when using `--csv` \
+or `--resolve`, the output will switch to CSV format.
+
+When outputting CSV, entries for unknown tunnel IDs will be an empty string, \
+rather than an error."
 )]
 #[clap(group(
     ArgGroup::new("ids")
@@ -17,21 +21,64 @@ and any unknown tunnel IDs will result in an empty string.",
         .args(&["tunnel-server-ids", "all"])
 ))]
 pub struct CliParser {
-    /// Derive one or more caBLE tunnel server hostnames by tunnel server ID.
-    #[clap()]
+    /// One or more tunnel server IDs.
+    #[clap(value_name = "ID", value_hint = ValueHint::Other)]
     pub tunnel_server_ids: Vec<u16>,
 
-    /// Lists all possible caBLE tunnel server domain names.
+    /// Derives all 65,536 possible tunnel server hostnames.
     #[clap(short, long)]
     pub all: bool,
+
+    /// Enable CSV output mode.
+    ///
+    /// This is automatically enabled when requesting multiple (or all) tunnel
+    /// server IDs, or when using `--resolve`.
+    #[clap(short, long)]
+    pub csv: bool,
+
+    /// Resolve tunnel server hostnames to IP addresses.
+    ///
+    /// This generally results in network traffic, so will be slow.
+    #[clap(short, long)]
+    pub resolve: bool,
 }
 
-fn print_domains(i: impl Iterator<Item = u16>) {
+/// Resolves a hostname to (an) IP address(es) using the system resolver,
+/// and return it as a comma-separated list.
+///
+/// Returns [None] on resolution failure or no results.
+fn resolver(hostname: &str) -> Option<String> {
+    (hostname, 443).to_socket_addrs().ok().and_then(|addrs| {
+        let mut o: String = addrs
+            .map(|addr| format!("{},", addr.ip().to_string()))
+            .collect();
+        o.pop();
+
+        if o.is_empty() {
+            return None;
+        }
+
+        Some(o)
+    })
+}
+
+/// Prints caBLE tunnel server hostnames in CSV format.
+fn print_hostnames(i: impl Iterator<Item = u16>, resolve: bool) {
     for domain_id in i {
-        println!(
-            "{domain_id},{:?}",
-            get_domain(domain_id).unwrap_or_default()
-        );
+        let domain = get_domain(domain_id);
+        let addrs = if resolve {
+            domain.as_deref().and_then(resolver)
+        } else {
+            None
+        }
+        .unwrap_or_default();
+        let domain = domain.unwrap_or_default();
+
+        if resolve {
+            println!("{domain_id},{domain:?},{addrs:?}",);
+        } else {
+            println!("{domain_id},{domain:?}",);
+        }
     }
 }
 
@@ -39,23 +86,28 @@ fn main() {
     let opt = CliParser::parse();
     tracing_subscriber::fmt::init();
 
-    if opt.tunnel_server_ids.len() == 1 {
+    if opt.tunnel_server_ids.len() == 1 && !(opt.resolve || opt.csv) {
         let domain_id = opt.tunnel_server_ids[0];
         match get_domain(domain_id) {
             Some(d) => println!("{d}"),
             None => CliParser::command()
                 .error(
-                    clap::ErrorKind::InvalidValue,
-                    format!("unknown domain ID: {domain_id}"),
+                    clap::ErrorKind::ValueValidation,
+                    format!("unknown tunnel server ID: {domain_id}"),
                 )
                 .exit(),
         }
     } else {
-        println!("tunnel_server_id,hostname");
-        if opt.all {
-            print_domains(u16::MIN..=u16::MAX);
+        if opt.resolve {
+            println!("tunnel_server_id,hostname,addrs");
         } else {
-            print_domains(opt.tunnel_server_ids.into_iter());
+            println!("tunnel_server_id,hostname");
+        }
+
+        if opt.all {
+            print_hostnames(u16::MIN..=u16::MAX, opt.resolve);
+        } else {
+            print_hostnames(opt.tunnel_server_ids.into_iter(), opt.resolve);
         }
     }
 }
