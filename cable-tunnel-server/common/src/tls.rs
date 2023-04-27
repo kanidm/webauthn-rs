85e1222bb475475f0afd6c87778566d00941d070
Michael Farrell <micolous+git@gmail.com>
2023-04-27 14:36:03 +1000

    fix another flag, attempt to pull spurious dep on PCSC

--- cable-tunnel-server/common/src/tls.rs
+++ cable-tunnel-server/common/src/tls.rs
@@ -106,7 +106,7 @@ impl ServerTransportProtocol {
 #[derive(Debug, Clone, Args)]
 pub struct BackendClientOptions {
     /// Uses unencrypted HTTP to connect to backend tasks, rather than HTTPS.
-    #[clap(long, conflicts_with_all(&["trusted-ca", "domain"]))]
+    #[clap(long, conflicts_with_all(&["trusted_ca", "domain"]))]
     insecure_http_backend: bool,
 
     /// Public key of the root CA to trust when connecting to a backend task,
