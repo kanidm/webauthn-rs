98a2bb6f89ba604e564247468725115237facfb9
Michael Farrell <micolous+git@gmail.com>
2023-07-04 17:15:31 +1000

    migrate from winres to embed-resource

--- fido-key-manager/build.rs
+++ fido-key-manager/build.rs
@@ -1,9 +1,7 @@
 fn main() {
-    #[cfg(windows)]
-    {
-        winres::WindowsResource::new()
-            .set_manifest_file("manifest.xml")
-            .compile()
-            .unwrap();
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+
+    if !cfg!(disable_windows_manifest) && target_os == "windows" {
+        embed_resource::compile("windows/fido-key-manager.rc", embed_resource::NONE);
     }
 }
