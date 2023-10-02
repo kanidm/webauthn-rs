c20e83f4aae635682b8b2f2fa9fca8534fe32d35
micolous <micolous+gh@gmail.com>
2023-10-03 09:18:41 +1000

    Add "HumanBinaryData" as an alternative to "Base64UrlSafeData" (#354)
    * Add "HumanBinaryData" as alternative to "Base64UrlSafeData" (#352)
    
    * Add bytes support to Base64UrlSafeData, and copy across the tests
    
    * Rework the docs
    
    * dedupe functionality into macros, make tests consistent
    
    * more tests and conversions
    
    * move Borrow impl into common
    
    * add some more vec features
    
    * fix clippy

--- base64urlsafedata/src/lib.rs
+++ base64urlsafedata/src/lib.rs
@@ -1,7 +1,76 @@
-//! Base64 data that encodes to Base64 UrlSafe, but can decode from multiple
-//! base64 implementations to account for various clients and libraries. Compatible
-//! with serde.
-
+//! Wrappers for `Vec<u8>` to make Serde serialise and deserialise as URL-safe,
+//! non-padded Base64 (per [RFC 4648 §5][sec5]).
+//!
+//! ## Serialisation behaviour
+//!
+//! * [`Base64UrlSafeData`] always serialises to URL-safe, non-padded Base64.
+//!
+//! * [`HumanBinaryData`] only serialises to URL-safe, non-padded Base64 when
+//!   using a [human-readable format][0].
+//!
+//!   Otherwise, it serialises as a "bytes"-like type (like [`serde_bytes`][1]).
+//!
+//!   This feature is new in `base64urlsafe` v0.1.4.
+//!
+//! By comparison, Serde's default behaviour is to serialise `Vec<u8>` as a
+//! sequence of integers. This is a problem for many formats:
+//!
+//! * `serde_cbor` encodes as an `array`, rather than a `bytes`. This uses
+//!   zig-zag encoded integers for values > `0x1F`, which averages about 1.88
+//!   bytes per byte assuming an equal distribution of values.
+//!
+//! * `serde_json` encodes as an `Array<Number>`, which averages 3.55 bytes per
+//!   byte without whitespace.
+//!
+//! Using Base64 encoding averages 1.33 bytes per byte, and most formats pass
+//! strings nearly-verbatim.
+//!
+//! ## Deserialisation behaviour
+//!
+//! Both types will deserialise multiple formats, provided the format is
+//! self-describing (ie: [implements `deserialize_any`][5]):
+//!
+//! * Bytes types are passed as-is (new in v0.1.4).
+//!
+//!   [`HumanBinaryData`] produces this for [non-human-readable formats][0].
+//!
+//! * Sequences of integers are passed as-is.
+//!
+//!   Serde's default `Vec<u8>` serialiser produces this for many formats.
+//!
+//! * Strings are decoded Base64 per [RFC 4648 §5 (URL-safe)][sec5] or
+//!   [§4 (standard)][sec4], with optional padding.
+//!
+//!   [`Base64UrlSafeData`] produces this for all formats, and
+//!   [`HumanBinaryData`] produces this for [human-readable formats][0]. This
+//!   should also be compatible with many other serialisers.
+//!
+//! ## Migrating from `Base64UrlSafeData` to `HumanBinaryData`
+//!
+//! [`Base64UrlSafeData`] always uses Base64 encoding, which isn't optimal for
+//! many binary formats. For that reason, it's a good idea to migrate to
+//! [`HumanBinaryData`] if you're using a binary format.
+//!
+//! However, you'll need to make sure *all* readers using [`Base64UrlSafeData`]
+//! are on `base64urlsafedata` v0.1.4 or later before switching *anything* to
+//! [`HumanBinaryData`]. Otherwise, they'll not be able to read any data in the
+//! new format!
+//!
+//! Once they're all migrated across, you can start issuing writes in the new
+//! format. It's a good idea to slowly roll out the change, in case you discover
+//! something has been left behind.
+//!
+//! ## Alternatives
+//!
+//! * [`serde_bytes`][1], which implements efficient coding of `Vec<u8>`
+//!   [for non-human-readable formats only][2].
+//!
+//! [0]: https://docs.rs/serde/latest/serde/trait.Serializer.html#method.is_human_readable
+//! [1]: https://docs.rs/serde_bytes
+//! [2]: https://github.com/serde-rs/bytes/issues/37
+//! [5]: https://serde.rs/impl-deserialize.html
+//! [sec4]: https://datatracker.ietf.org/doc/html/rfc4648#section-4
+//! [sec5]: https://datatracker.ietf.org/doc/html/rfc4648#section-5
 #![deny(warnings)]
 #![warn(unused_extern_crates)]
 #![deny(clippy::todo)]
@@ -14,15 +83,24 @@
 #![deny(clippy::needless_pass_by_value)]
 #![deny(clippy::trivially_copy_pass_by_ref)]
 
+#[macro_use]
+extern crate paste;
+
+#[macro_use]
+mod common;
+mod human;
+#[cfg(test)]
+mod tests;
+
+pub use crate::human::HumanBinaryData;
+
 use base64::{
     engine::general_purpose::{
         GeneralPurpose, STANDARD, STANDARD_NO_PAD, URL_SAFE, URL_SAFE_NO_PAD,
     },
     Engine,
 };
-use serde::de::{Error, SeqAccess, Unexpected, Visitor};
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
-use std::borrow::Borrow;
+use serde::{Serialize, Serializer};
 use std::convert::TryFrom;
 use std::fmt;
 use std::hash::Hash;
@@ -30,40 +108,43 @@ use std::hash::Hash;
 static ALLOWED_DECODING_FORMATS: &[GeneralPurpose] =
     &[URL_SAFE_NO_PAD, URL_SAFE, STANDARD, STANDARD_NO_PAD];
 
+/// Serde wrapper for `Vec<u8>` which always emits URL-safe, non-padded Base64,
+/// and accepts Base64 and binary formats.
+///
+/// * Deserialisation is described in the [module documentation][crate].
+///
+/// * Serialisation *always* emits URL-safe, non-padded Base64 (per
+///   [RFC 4648 §5][sec5]).
+///
+///   Unlike [`HumanBinaryData`], this happens *regardless* of whether the
+///   underlying serialisation format is [human readable][0]. If you're
+///   serialising to [non-human-readable formats][0], you should consider
+///   [migrating to `HumanBinaryData`][crate].
+///
+/// Otherwise, this type should work as much like a `Vec<u8>` as possible.
+///
+/// [0]: https://docs.rs/serde/latest/serde/trait.Serializer.html#method.is_human_readable
+/// [sec5]: https://datatracker.ietf.org/doc/html/rfc4648#section-5
 #[derive(Debug, Clone, PartialEq, Eq, Ord, PartialOrd, Hash)]
-/// A container for binary that should be base64 encoded in serialisation. In reverse
-/// when deserializing, will decode from many different types of base64 possible.
 pub struct Base64UrlSafeData(pub Vec<u8>);
+
+common_impls!(Base64UrlSafeData);
+
+impl From<HumanBinaryData> for Base64UrlSafeData {
+    fn from(value: HumanBinaryData) -> Self {
+        Self(value.into())
+    }
+}
+
+impl PartialEq<HumanBinaryData> for Base64UrlSafeData {
+    fn eq(&self, other: &HumanBinaryData) -> bool {
+        self.0.eq(other)
+    }
+}
 
 impl fmt::Display for Base64UrlSafeData {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", URL_SAFE_NO_PAD.encode(self))
-    }
-}
-
-impl Borrow<[u8]> for Base64UrlSafeData {
-    fn borrow(&self) -> &[u8] {
-        self.0.as_slice()
-    }
-}
-
-impl From<Vec<u8>> for Base64UrlSafeData {
-    fn from(v: Vec<u8>) -> Base64UrlSafeData {
-        Base64UrlSafeData(v)
-    }
-}
-
-// We have to allow this because we can't implement a trait on an external type
-#[allow(clippy::from_over_into)]
-impl Into<Vec<u8>> for Base64UrlSafeData {
-    fn into(self) -> Vec<u8> {
-        self.0
-    }
-}
-
-impl AsRef<[u8]> for Base64UrlSafeData {
-    fn as_ref(&self) -> &[u8] {
-        &self.0
     }
 }
 
@@ -80,56 +161,6 @@ impl TryFrom<&str> for Base64UrlSafeData {
     }
 }
 
-struct Base64UrlSafeDataVisitor;
-
-impl<'de> Visitor<'de> for Base64UrlSafeDataVisitor {
-    type Value = Base64UrlSafeData;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(formatter, "a base64 url encoded string")
-    }
-
-    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-    where
-        E: Error,
-    {
-        // Forgive alt base64 decoding formats
-        for config in ALLOWED_DECODING_FORMATS {
-            if let Ok(data) = config.decode(v) {
-                return Ok(Base64UrlSafeData(data));
-            }
-        }
-
-        Err(serde::de::Error::invalid_value(Unexpected::Str(v), &self))
-    }
-
-    fn visit_seq<A>(self, mut v: A) -> Result<Self::Value, A::Error>
-    where
-        A: SeqAccess<'de>,
-    {
-        let mut data = if let Some(sz) = v.size_hint() {
-            Vec::with_capacity(sz)
-        } else {
-            Vec::new()
-        };
-
-        while let Some(i) = v.next_element()? {
-            data.push(i)
-        }
-        Ok(Base64UrlSafeData(data))
-    }
-}
-
-impl<'de> Deserialize<'de> for Base64UrlSafeData {
-    fn deserialize<D>(deserializer: D) -> Result<Self, <D as Deserializer<'de>>::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        // Was previously _str
-        deserializer.deserialize_any(Base64UrlSafeDataVisitor)
-    }
-}
-
 impl Serialize for Base64UrlSafeData {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
@@ -137,24 +168,5 @@ impl Serialize for Base64UrlSafeData {
     {
         let encoded = URL_SAFE_NO_PAD.encode(self);
         serializer.serialize_str(&encoded)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::Base64UrlSafeData;
-    use std::convert::TryFrom;
-
-    #[test]
-    fn test_try_from() {
-        assert!(Base64UrlSafeData::try_from("aGVsbG8=").is_ok());
-        assert!(Base64UrlSafeData::try_from("abcdefghij").is_err());
-    }
-
-    #[test]
-    fn test_try_from_json() {
-        // let _: Base64UrlSafeData = serde_json::from_str("\"aGVsbG8=\"")
-        // .expect("Invalid Data");
-        assert!(serde_json::from_str::<Base64UrlSafeData>("[0,1,2,3]").is_ok());
     }
 }
