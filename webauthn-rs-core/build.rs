0c9e080689d425435895b0ffcd73c616cb13b192
Michael Farrell <micolous+git@gmail.com>
2024-02-12 11:34:36 +1000

    Add explicit OpenSSL version checks to attestation-ca and webauthn-rs-core

---
+++ webauthn-rs-core/build.rs
@@ -0,0 +1,21 @@
+use openssl::version::{number, version};
+
+const OPENSSL_DOC: &str = "https://github.com/kanidm/webauthn-rs/blob/master/OpenSSL.md";
+
+fn main() {
+    // LibreSSL reports as OpenSSL v2.
+    if number() < 0x2_00_00_00_0 {
+        println!(
+            r#"
+Your version of OpenSSL is out of date, and not supported by this library.
+
+Please upgrade to OpenSSL v3.0.0 or later.
+
+More info: {OPENSSL_DOC}
+OpenSSL version string: {}
+"#,
+            version(),
+        );
+        panic!("The installed version of OpenSSL is unusable.");
+    }
+}
