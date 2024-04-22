c992b47d9bf33538cac332e08431d05b321cd1d7
William Brown <william@blackhats.net.au>
2024-04-22 13:21:34 +1000

    key manager

--- fido-key-manager/src/main.rs
+++ fido-key-manager/src/main.rs
@@ -583,7 +583,7 @@ async fn main() {
                     match &public_key.key {
                         COSEKeyType::EC_OKP(okp) => {
                             println!("  Octet key pair, curve {:?}", okp.curve);
-                            println!("    X-coordinate: {}", hex::encode(&okp.x.0));
+                            println!("    X-coordinate: {}", hex::encode(&okp.x));
                         }
                         COSEKeyType::EC_EC2(ec) => {
                             println!("  Elliptic curve key, curve {:?}", ec.curve);
