c6114e9342f9b619ec8d67eb1cdeae3306e3a29b
William Brown <william@blackhats.net.au>
2024-11-22 13:57:13 +1000

    Progress

--- fido-key-manager/build.rs
+++ fido-key-manager/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
 
-    if !cfg!(disable_windows_manifest) && target_os == "windows" {
+    if !cfg!(feature = "disable_windows_manifest") && target_os == "windows" {
         embed_resource::compile("windows/fido-key-manager.rc", embed_resource::NONE);
     }
 }
