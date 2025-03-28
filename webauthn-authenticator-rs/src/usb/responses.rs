661f4b9a8f0593466b1b8735ee90da90a8db79c5
Luca BRUNO <lucab@lucabruno.net>
2025-03-28 11:37:32 +0100

    authenticator/usb: detect wink capability
    This adds USB-HID logic for detecting `CTAPHID_WINK` support.
    
    Ref: https://fidoalliance.org/specs/fido-v2.0-ps-20190130/fido-client-to-authenticator-protocol-v2.0-ps-20190130.html#usb-hid-init

--- webauthn-authenticator-rs/src/usb/responses.rs
+++ webauthn-authenticator-rs/src/usb/responses.rs
@@ -11,6 +11,7 @@ use crate::transport::{
 use crate::usb::framing::U2FHIDFrame;
 use crate::usb::*;
 
+const CAPABILITY_WINK: u8 = 0x01;
 const CAPABILITY_CBOR: u8 = 0x04;
 const CAPABILITY_NMSG: u8 = 0x08;
 
@@ -29,14 +30,19 @@ pub struct InitResponse {
 }
 
 impl InitResponse {
-    /// `true` if the device suports CTAPv1 / U2F protocol
+    /// `true` if the device supports CTAPv1 / U2F protocol.
     pub fn supports_ctap1(&self) -> bool {
         self.capabilities & CAPABILITY_NMSG == 0
     }
 
-    /// `true` if the device suports CTAPv2 / CBOR protocol
+    /// `true` if the device supports CTAPv2 / CBOR protocol.
     pub fn supports_ctap2(&self) -> bool {
         self.capabilities & CAPABILITY_CBOR > 0
+    }
+
+    /// `true` if the device supports wink function.
+    pub fn supports_wink(&self) -> bool {
+        self.capabilities & CAPABILITY_WINK > 0
     }
 }
 
@@ -169,6 +175,7 @@ mod tests {
         if let Response::Init(r) = r {
             assert!(r.supports_ctap1());
             assert!(r.supports_ctap2());
+            assert!(r.supports_wink());
         } else {
             panic!("bad response");
         }
@@ -201,6 +208,7 @@ mod tests {
         if let Response::Init(r) = r {
             assert!(!r.supports_ctap1());
             assert!(r.supports_ctap2());
+            assert!(r.supports_wink());
         } else {
             panic!("bad response");
         }
@@ -227,6 +235,7 @@ mod tests {
         if let Response::Init(r) = r {
             assert!(r.supports_ctap1());
             assert!(!r.supports_ctap2());
+            assert!(!r.supports_wink());
         } else {
             panic!("bad response");
         }
