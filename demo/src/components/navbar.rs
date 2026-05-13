91d5695e1a2704e0a6cfdddfb55d417c797aa96d
Michael Farrell <micolous+git@gmail.com>
2026-05-13 18:51:43 +1000

    make navbar brand clickable

--- demo/src/components/navbar.rs
+++ demo/src/components/navbar.rs
@@ -7,7 +7,7 @@ pub fn Navbar() -> impl IntoView {
     view! {
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-                <span class="navbar-brand mb-0 h1">"webauthn-rs demo"</span>
+                <a href="/" class="navbar-brand mb-0 h1">"webauthn-rs demo"</a>
 
                 <button
                     class="navbar-toggler"
