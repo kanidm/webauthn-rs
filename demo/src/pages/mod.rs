cfc848460ced70a1fb740f09e4060dc51fd5047f
Michael Farrell <micolous+git@gmail.com>
2026-05-13 15:16:58 +1000

    wip: registration flow

--- demo/src/pages/mod.rs
+++ demo/src/pages/mod.rs
@@ -1,3 +1,6 @@
 mod home;
+mod login;
+mod not_found;
+mod register;
 
-pub use self::home::HomePage;
+pub use self::{home::HomePage, login::LoginPage, not_found::NotFoundPage, register::RegisterPage};
