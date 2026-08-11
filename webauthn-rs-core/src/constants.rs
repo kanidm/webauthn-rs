e9fd1e1be23a50ce3383a698eeef62a2b91976af
Brian Koziel <brian@useideem.com>
2026-08-10 22:36:55 -0500

    Limited credentialIdLength to 1023 bytes in acd_parser

--- webauthn-rs-core/src/constants.rs
+++ webauthn-rs-core/src/constants.rs
@@ -1,2 +1,5 @@
 // Can this ever change?
 pub const CHALLENGE_SIZE_BYTES: usize = 32;
+
+// WebAuthn L3 §6.5.1 (Attested Credential Data): credentialIdLength "Value MUST be <= 1023"
+pub const CREDENTIAL_ID_MAX_LENGTH: u16 = 1023;
