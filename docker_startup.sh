d7ac6b61b79cace643c344d5c168b666c42590c7
Michael Farrell <micolous+git@gmail.com>
2026-09-21 13:28:52 +1000

    Fix startup script

--- docker_startup.sh
+++ docker_startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-if [ -z "${SECRET_KEY}" ]
+if [ -z "${SECRET_KEY}" ]; then
     echo "WARNING: SECRET_KEY was not specified, randomly generating a key."
     echo "WARNING: All cookies will be invalidated on container restart!"
     export SECRET_KEY="$(openssl rand -hex 32)"
